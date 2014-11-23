c4fde8af9bee9f5fc5ec6e38ee5e31f543e3cef9
scarletsky <scarletsky1025@gmail.com>
2014-11-23 14:24:54 +0800

    chore: update fate

--- data/fate.js
+++ data/fate.js
@@ -4,12 +4,26 @@ exports.fate = {
   info: '传说中，圣杯是能够实现拥有者愿望的宝物。为了追求圣杯的力量，7位魔术师各自召唤英灵，展开争夺圣杯的战斗，这就是圣杯战争。',
   cover: '',
   characters: [
+    // 士郎
+    {
+      name: '卫宫士郎',
+      alias: ['士郎'],
+      info: '游戏及同名动漫《Fate/Stay night》中的男主角，穗群原学园高中部二年级学生，10年前冬木市第四次圣杯战争引起的火灾中的幸存者，由于幼年对战争的惨痛记忆和受到养父卫宫切嗣的影响，立志拯救他人。在第五次圣杯战争中作为Saber的Master出场。'
+    },
+    // saber
     {
       name: '阿尔托利亚·潘德拉贡',
       alias: ['Saber'],
       info: '身份为古不列颠传说中的亚瑟王。性格忠诚正直，谦逊有礼，个性认真。因有圣剑Excalibur的传承，在第四、五次圣杯战争中以“Saber”职阶被召唤到现世。',
       avatar: ''
     },
+    // archer
+    {
+      name: '英灵卫宫',
+      alias: ['Emiya', 'Archer', '红A'],
+      info: '参加《Fate/Stay night》所描述的第五次圣杯战争的Archer为卫宫士郎未来的英灵化化身，昵称为红A。'
+    },
+    // 伊莉雅
     {
       name: '依莉雅斯菲尔·冯·爱因兹贝伦',
       alias: ['イリヤ', '伊莉雅'],
@@ -30,6 +44,16 @@ exports.fate = {
       reference: 'Fate/stay night UBW第3话',
       scene: 'http://aniquo.qiniudn.com/images/quoteScene/01adcd793dbb01787b73f4815222bbfb',
       characters: ['伊莉雅']
-    }
+    },
+    {
+      quote: 'Archer：没有战斗意义的卫宫士郎就死在这里吧。不是为了自己，而是为了他人战斗，这只是伪善罢了。你期待的不是胜利，而是和平吧。那种东西，在这个世界上根本不存在。\n\n士郎：你说什么...\n\nArcher：永别了，你就抱着理想溺死吧。',
+      reference: 'Fate/stay night UBW第7话',
+      characters: ['Archer', '士郎']
+    },
+    {
+      quote: '不会再重复今天晚上这样的失败了，一味逃避什么都改变不了。更何况，为了减少牺牲者而去牺牲一些人，我绝对不想输给说出这种话的人。',
+      reference: 'Fate/stay night UBW第7话',
+      characters: ['士郎']
+    },
   ]
 }
