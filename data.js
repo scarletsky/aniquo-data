4421cc789c6a769ad6c1a3bb4807b7bb9972d8da
scarletsky <scarletsky1025@gmail.com>
2015-04-27 00:26:02 +0800

    chore: for new fields in source model

--- data.js
+++ data.js
@@ -15,192 +15,205 @@ var config = require('./config/config')[env];
 
 var data = [];
 fs.readdirSync('./data/').forEach(function (filename) {
-  var name = filename.replace('.js', '');
-  var fileData = require('./data/' + name)[name];
-  data.push(fileData);
+    var name = filename.replace('.js', '');
+    var fileData = require('./data/' + name)[name];
+    data.push(fileData);
 });
 
 var connect = function () {
-  var options = {
-    server: {
-      socketOptions: {
-        keepAlive: 1
-      }
-    }
-  };
-  mongoose.connect(config.db, options);
+    var options = {
+        server: {
+            socketOptions: {
+                keepAlive: 1
+            }
+        }
+    };
+    mongoose.connect(config.db, options);
 };
 connect();
 
 async.waterfall([
-  // remove index
-  function (callback) {
-    shell.exec('curl -XDELETE "http://localhost:9200/sources/"', {silent:true});
-    shell.exec('curl -XDELETE "http://localhost:9200/characters/"', {silent:true});
-    shell.exec('curl -XDELETE "http://localhost:9200/quotes/"', {silent:true});
-    console.log('indexes remove successfully');
-    callback(null);
-  },
-  // remove useres
-  function (callback) {
-    User.remove(function (err) {
-      console.log('users remove successfully');
-      callback(null);
-    });
-  },
-  // remove sources
-  function (callback) {
-    Source.remove(function (err) {
-      console.log('sources remove successfully');
-      callback(null);
-    });
-  },
-  // remove characters
-  function (callback) {
-    Character.remove(function (err) {
-      console.log('characters remove successfully');
-      callback(null);
-    });
-  },
-  //remove quotes
-  function (callback) {
-    Quote.remove(function (err) {
-      console.log('quotes remove successfully');
-      callback(null);
-    });
-  },
-  // insert users
-  function (callback) {
-    var userX = {username: 'hello', password: 'world'};
-    var user = new User(userX);
-    user.save(function () {
-      console.log('user ' + userX.username + ' init successfully!');
-      callback(null, user);
-    });
-  },
-  // insert data
-  function (user, callback) {
-
-    // insert sources
-    async.each(data, function (object, callback) {
-
-      var sourceX = {
-        name: object.name,
-        alias: object.alias,
-        info: object.info,
-        cover: object.cover,
-        contributorId: user._id
-      };
-      var newSource = new Source(sourceX);
-      newSource.save();
-
-      (function (source, characters) {
-
-        // insert characters
-        async.each(characters, function (character, callback) {
-          var characterX = {
-            name: character.name,
-            alias: character.alias,
-            info: character.info,
-            avatar: character.avatar,
-            sourceId: newSource._id,
-            quotesCount: 0,
-            contributorId: user._id
-          };
-
-          var newCharacter = new Character(characterX);
-          newCharacter.save(function (err) {
-            callback();
-          });
-
-        }, function (err) {
-          if (err) {
-            console.log(err);
-          } else {
-            ep.emit('source' + source._id + 'charactersok');
-          }
+    // remove index
+    function (callback) {
+        shell.exec('curl -XDELETE "http://localhost:9200/sources/"', {silent:true});
+        shell.exec('curl -XDELETE "http://localhost:9200/characters/"', {silent:true});
+        shell.exec('curl -XDELETE "http://localhost:9200/quotes/"', {silent:true});
+        console.log('indexes remove successfully');
+        callback(null);
+    },
+    // remove useres
+    function (callback) {
+        User.remove(function (err) {
+            console.log('users remove successfully');
+            callback(null);
         });
+    },
+    // remove sources
+    function (callback) {
+        Source.remove(function (err) {
+            console.log('sources remove successfully');
+            callback(null);
+        });
+    },
+    // remove characters
+    function (callback) {
+        Character.remove(function (err) {
+            console.log('characters remove successfully');
+            callback(null);
+        });
+    },
+    //remove quotes
+    function (callback) {
+        Quote.remove(function (err) {
+            console.log('quotes remove successfully');
+            callback(null);
+        });
+    },
+    // insert users
+    function (callback) {
+        var userX = {username: 'hello', password: 'world'};
+        var user = new User(userX);
+        user.save(function () {
+            console.log('user ' + userX.username + ' init successfully!');
+            callback(null, user);
+        });
+    },
+    // insert data
+    function (user, callback) {
 
-      })(newSource, object.characters);
+        // insert sources
+        async.each(data, function (object, callback) {
 
-
-      (function (source, quotes) {
-
-        // insert quotes
-        ep.once('source' + source._id + 'charactersok', function () {
-
-          async.each(quotes, function (quote, callback) {
-
-            var characterIds = [];
-            var quoteX = {
-              quote: quote.quote,
-              likeCount: 0,
-              scene: quote.scene,
-              reference: quote.reference,
-              contributorId: user._id
+            var sourceX = {
+                name: object.name,
+                alias: object.alias,
+                info: object.info,
+                cover: object.cover,
+                viewsCount: object.viewsCount || Math.ceil(Math.random() * 10) * Math.ceil(Math.random() * 10),
+                contributorId: user._id
             };
-            var newQuote = new Quote(quoteX);
+            var newSource = new Source(sourceX);
+            newSource.save();
 
-            // query quote's characters
-            async.map(quote.characters, function (characterName, callback) {
+            (function (source, characters) {
 
-              Character
-                .findOneAndUpdate({
-                  sourceId: source._id,
-                  $or: [
-                    {name: characterName},
-                    {alias: characterName}
-                  ]
-                }, {
-                  $inc: {
-                    quotesCount: 1
-                  }
-                })
-                .exec(function (err, character) {
-                  if (err) {
-                    return console.log(err);
-                  } else {
-                    callback(null, character._id);
-                  }
+                // insert characters
+                async.each(characters, function (character, callback) {
+                    var characterX = {
+                        name: character.name,
+                        alias: character.alias,
+                        info: character.info,
+                        avatar: character.avatar,
+                        sourceId: newSource._id,
+                        quotesCount: 0,
+                        contributorId: user._id
+                    };
+
+                    var newCharacter = new Character(characterX);
+                    newCharacter.save(function (err) {
+                        newSource.update({
+                            $inc: {
+                                charactersCount: 1
+                            }
+                        }, function(err, doc) {
+                            callback();
+                        });
+                    });
+
+                }, function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        ep.emit('source' + source._id + 'charactersok');
+                    }
                 });
 
-            }, function (err, characterIds) {
-              newQuote.characterIds = characterIds;
-              newQuote.save(function () {
-                callback(null); // what's this?
-              });
+            })(newSource, object.characters);
 
-            }); // end of async on mapping characterIds of quote
 
-          }, function () {
+            (function (source, quotes) {
 
-            callback(null); // what's this?
+                // insert quotes
+                ep.once('source' + source._id + 'charactersok', function () {
 
-          }); // end of async on inserting quotes
+                    async.each(quotes, function (quote, callback) {
 
-        }); // end of event proxy
+                        var characterIds = [];
+                        var quoteX = {
+                            quote: quote.quote,
+                            likeCount: 0,
+                            scene: quote.scene,
+                            reference: quote.reference,
+                            contributorId: user._id
+                        };
+                        var newQuote = new Quote(quoteX);
 
-      })(newSource, object.quotes);
+                        // query quote's characters
+                        async.map(quote.characters, function (characterName, callback) {
 
-    }, function (err) {
-      // finish!
-      callback(null);
-    });
+                            Character
+                                .findOneAndUpdate({
+                                    sourceId: source._id,
+                                    $or: [
+                                        {name: characterName},
+                                        {alias: characterName}
+                                    ]
+                                }, {
+                                    $inc: {
+                                        quotesCount: 1
+                                    }
+                                })
+                                .exec(function (err, character) {
+                                    if (err) {
+                                        return console.log(err);
+                                    } else {
+                                        callback(null, character._id);
+                                    }
+                                });
 
-  }
+                        }, function (err, characterIds) {
+                            newQuote.characterIds = characterIds;
+                            newQuote.save(function () {
+                                newSource.update({
+                                    $inc: {
+                                        quotesCount: 1
+                                    }
+                                }, function(err) {
+                                    callback(null); // what's this?
+                                });
+                            });
+
+                        }); // end of async on mapping characterIds of quote
+
+                    }, function () {
+
+                        callback(null); // what's this?
+
+                    }); // end of async on inserting quotes
+
+                }); // end of event proxy
+
+            })(newSource, object.quotes);
+
+        }, function (err) {
+            // finish!
+            callback(null);
+        });
+
+    }
 // finish
 ], function (err, results) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
 
-  console.log('----- insert finished -----');
-  process.exit(0);
+    console.log('----- insert finished -----');
+    process.exit(0);
 
-  // setTimeout(function () {
-  //   console.log('----- adding index -----');
-  //   shell.exec('node addIndex.js');
-  //   console.log('----- adding index finished -----');
-  // }, 1000);
+    // setTimeout(function () {
+    //   console.log('----- adding index -----');
+    //   shell.exec('node addIndex.js');
+    //   console.log('----- adding index finished -----');
+    // }, 1000);
 });
